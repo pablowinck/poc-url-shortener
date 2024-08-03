ede2cd7cb11a308c512416a6e3df30a4d9e7bc6d
pablowinck <contato@pablowinter.com.br>
2024-08-03 09:47:55 -0300

    del wpp service

--- src/main/java/com/github/pablowinck/UrlController.java
+++ src/main/java/com/github/pablowinck/UrlController.java
@@ -10,11 +10,8 @@ public class UrlController {
 
     private final UrlService urlService;
 
-    private final WhatsappService whatsappService;
-
-    public UrlController(UrlService urlService, WhatsappService whatsappService) {
+    public UrlController(UrlService urlService) {
         this.urlService = urlService;
-        this.whatsappService = whatsappService;
     }
 
     @PostMapping("/shorten")
@@ -28,12 +25,6 @@ public class UrlController {
         return urlService.getOriginalUrl(shortUrl)
                 .map(url -> new RedirectView(url.getOriginalUrl()))
                 .orElseThrow(() -> new RuntimeException("URL not found"));
-    }
-
-    @GetMapping("/whatsapp/{shortUrl}")
-    @ResponseBody
-    public String getWhatsappLink(@PathVariable String shortUrl) {
-        return whatsappService.generateWhatsappLink(shortUrl);
     }
 
 }
